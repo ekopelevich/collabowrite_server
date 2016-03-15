cbf75bcdf254dd72f541fd2e6db762cb8789a1c1
Elana Kopelevich <ekopelevich@gmail.com>
2016-03-15 11:24:09 -0600

    post stories route functioning

--- routes/stories.js
+++ routes/stories.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
+var moment = require('moment');
 
 router.get('/:story_id', function(req, res, next) {
   knex('stories').select().where('id', req.params.story_id)
@@ -18,16 +19,36 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   console.log(req.body);
-  var story = {};
+  var story = {
+    owner_id: 1,
+    title: req.body.title,
+    start_date: moment().format(),
+    summary: req.body.summary,
+    edit_lock: 0,
+    genre_id: req.body.genre,
+    checkout_time: moment().format(),
+    state_id: 1
+  };
 
   knex('stories').returning('id').insert(story).then(function(ids) {
     story.id = ids[0];
+    console.log('story', story)
     res.status(201).send(story);
   });
 });
 
 router.put('/:story_id', function(req, res, next) {
-  var story = {};
+  console.log(req.body);
+  var story = {
+    owner_id: 1,
+    title: req.body.title,
+    start_date: Date.now(),
+    summary: req.body.summary,
+    edit_lock: 0,
+    genre_id: req.body.genre,
+    checkout_time: Date.now(),
+    state_id: 1
+  };
 
   knex('stories').update(story)
   .where('id', req.params.story_id)
